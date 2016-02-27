fcb7e45366135a4991f26e39d93135fbfb654856
khrist14n <khrist14n@gmail.com>
2016-02-27 03:27:33 -0500

    proyecto dom y angularjs

--- git.sh
+++ git.sh
@@ -1,6 +1,15 @@
-sh delete.sh
 clear
 git reset HEAD *
-git add . -f
-git commit -am "proyecto inicial"
+git add app -f
+git add nbproject -f
+git add bower.json
+git  add contributors.txt
+git add delete.sh
+git add git.sh
+git add Gruntfile.js
+git add package.json
+git add readme.md
+git add README.md
+git add run.sh
+git commit -am "proyecto dom y angularjs"
 git push origin master
